a8723b9caf581bbf511cd95073505d9473907292
mrsamsonn <andrewsj.ahs@gmail.com>
2024-05-19 07:21:23 -0700

    pantry training set to pyTorch to Openvino

--- grocery.py
+++ grocery.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
 rf = Roboflow(api_key="18PZpI8vgWrbkC6iYYvG")
-project = rf.workspace("samrat-sahoo").project("groceries-6pfog")
-version = project.version(7)
+project = rf.workspace("yolo-1eope").project("pantry-wlooe")
+version = project.version(1)
 dataset = version.download("yolov8")
